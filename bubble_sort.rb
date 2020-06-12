3538090eff970cb5c9e22d93c80c2c4805afe567
MarilenaRoque <marilenas@id.uff.br>
2020-06-12 14:39:42 -0300

    Added README.md and finish bubble_sort
    Joyned bubble_sort and bubble_sort_by on a single version
    Added README.md model

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,23 +1,47 @@
-new_array = [ 1 , 6 , 9 , 5 , 8 , 2 ]
-
+test = -5
 
 def bubble_sort(new_array)
   flag = 0
   while flag == 0 do
     flag = 1 
     new_array.each_with_index do |numb, position|
-      puts "I am here"
       unless position == new_array.length-1
-        if numb > new_array[position+1]
+        test= yield(numb, new_array[position+1])
+        if test > 0
           new_array[position]=new_array[position+1] 
           new_array[position+1]=numb
           flag=0
         end
       end
-      puts new_array
     end
   end
 end
 
-bubble_sort(new_array)
-puts new_array.class
+def bubble_sort_by(new_array)
+  flag = 0
+  while flag == 0 do
+    flag = 1 
+    new_array.each_with_index do |numb, position|
+      unless position == new_array.length-1
+        test= yield(numb, new_array[position+1])
+        if test > 0
+          new_array[position]=new_array[position+1] 
+          new_array[position+1]=numb
+          flag=0
+        end
+      end
+    end
+  end
+end
+
+new_array =[1,2,5,8,1,8,8]
+bubble_sort(new_array) do |left,right| 
+  left - right
+end
+puts new_array
+
+new_array =["hi","hello","hey"]
+bubble_sort_by(new_array) do |left,right|
+  left.length - right.length
+end
+puts new_array
