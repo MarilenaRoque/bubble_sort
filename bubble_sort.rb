46fa20c98ec717563b6355fa26f3d38bdd4b0ca3
MarilenaRoque <marilenas@id.uff.br>
2020-06-12 18:39:35 -0300

    Fixing Return and yield
    Added a return value for the bubble_sort method
    Added a return value for the bubble_sort_by method
    Remove the yield for the bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -4,15 +4,14 @@ def bubble_sort(new_array)
     flag = 1
     new_array.each_with_index do |numb, position|
       next if position == new_array.length - 1
-
-      test = yield(numb, new_array[position + 1])
-      next unless test.positive?
-
-      new_array[position] = new_array[position + 1]
-      new_array[position + 1] = numb
-      flag = 0
+      if numb > new_array[position + 1]
+        new_array[position] = new_array[position + 1]
+        new_array[position + 1] = numb
+        flag = 0
+      end
     end
   end
+  new_array
 end
 
 def bubble_sort_by(new_array)
@@ -30,13 +29,8 @@ def bubble_sort_by(new_array)
       flag = 0
     end
   end
+  new_array
 end
-
-new_array = [1, 2, 5, 8, 1, 8, 8]
-bubble_sort(new_array) do |left, right|
-  left - right
-end
-puts new_array
 
 new_array = %w[hi hello hey]
 bubble_sort_by(new_array) do |left, right|
